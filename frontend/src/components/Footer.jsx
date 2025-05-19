c27ae39dc1fccd5f9983de8fe5833c236eeb6e28
MatthewTCC <165826800+MatthewWARTHUNDER@users.noreply.github.com>
2025-05-18 22:01:54 -0300

    update

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="bg-NavbarEfooter ">
+        <footer className="bg-NavbarEfooter">
             <div className="w-full max-w-screen-xl mx-auto p-4 py-6 lg:py-8">
                 <div className="flex flex-col md:flex-row md:justify-between gap-6">
+
                     <div className="flex flex-col items-center md:items-start">
                         <a href="/" className="flex flex-col items-center md:flex-row md:items-center gap-2">
                             <img src={logo} alt="Logo da empresa" className="h-20 md:h-28" />
@@ -13,9 +15,23 @@ const Footer = () => {
                         </a>
                     </div>
 
-                    <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 gap-6 text-center md:text-left">
+
+                    <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 text-center md:text-left">
+
                         <div>
-                            <h2 className="mb-4 text-sm font-semibold uppercase text-black">Entre em contato</h2>
+                            <h2 className="mb-4 text-sm font-semibold uppercase text-black">Legal</h2>
+                            <ul>
+                                <li>
+                                    <Link to="/Privacidade" className="text-black hover:text-dourado transition-colors">
+                                        Termos de Privacidade
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+
+                        {/* Contato */}
+                        <div>
+                            <h2 className="mb-4 text-sm font-semibold uppercase text-black">Contato</h2>
                             <ul>
                                 <li>
                                     <a href="https://wa.me/549999179427" className="text-black hover:text-dourado transition-colors">
@@ -24,12 +40,16 @@ const Footer = () => {
                                 </li>
                             </ul>
                         </div>
+
+                        {/* Mídias sociais */}
                         <div>
-                            <h2 className="mb-4 text-sm font-semibold uppercase text-black">Nossas mídias sociais</h2>
+                            <h2 className="mb-4 text-sm font-semibold uppercase text-black">Redes sociais</h2>
                             <ul>
                                 <li>
-                                    <a href="https://www.instagram.com/bem_me_quer_floresepresentes/?igsh=dmJnbTlucGN2ZW40#"
-                                        className="text-black hover:text-dourado transition-colors">
+                                    <a
+                                        href="https://www.instagram.com/bem_me_quer_floresepresentes/?igsh=dmJnbTlucGN2ZW40#"
+                                        className="text-black hover:text-dourado transition-colors"
+                                    >
                                         Instagram
                                     </a>
                                 </li>
@@ -40,7 +60,7 @@ const Footer = () => {
 
                 <hr className="my-6 border-black" />
 
-                <div className="text-center md:text-left">
+                <div className="text-center">
                     <span className="text-black font-medium">
                         © 2025 <a href="/" className="hover:underline">Bem-me-Quer™</a>. Todos os direitos reservados.
                     </span>
